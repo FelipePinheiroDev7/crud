4bd24133acca85b10b6d523848da2dc4209d25cc
Felipe Pinheiro <96586008+FelipePinheiroDev7@users.noreply.github.com>
2022-01-14 00:30:28 -0300

    proteção de dados
    proteção de dados

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('crud','root','Lipe2575',{
+const sequelize = new Sequelize('crud','root','000',{
     dialect:'mysql',
     host:'localhost'
 })
